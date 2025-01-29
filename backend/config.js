d5fe36dd57140a76124c119ba1a3d2e02f55cbda
Meetin82 <90614890+Meetin82@users.noreply.github.com>
2025-01-29 16:54:35 +0300

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    API_KEY: "JCBKT6S-Y1X4V09-HPBSDCX-WVEDRWY",
+    API_KEY: "YOUR_SECRET_KEY",
     BASE_URL_v1_4: "https://api.kinopoisk.dev/v1.4",
     BASE_URL_v1: "https://api.kinopoisk.dev/v1",
 };
